913a7c6f16435a5fa5d05717137b5f1e74e3a417
Glen Wallace <glen.wallace@praeses.com>
2015-06-26 10:50:45 -0500

    Caches a list of all location names upon request.

--- server.rb
+++ server.rb
@@ -5,11 +5,14 @@ require './src/services/drug_counts_importer.rb'
 require './src/services/drug_name_importer.rb'
 require './src/services/location_counts_importer.rb'
 
+require './src/services/location_name_importer.rb'
+
 set :public_folder, Proc.new { File.join(root, "www") }
 
 DRUG_COUNT_IMPORTER = Services::DrugCountsImporter.new
 DRUG_NAME_IMPORTER = Services::DrugNameImporter.new
 LOCATION_COUNT_IMPORTER = Services::DrugCountsImporter.new
+LOCATION_NAME_IMPORTER =  Services::LocationNameImporter.new
 
 get '/' do
   File.read(File.join('www', 'index.html'))
@@ -28,4 +31,9 @@ end
 get '/drug_name.csv' do
 	DRUG_NAME_IMPORTER.pull
 	DRUG_NAME_IMPORTER.to_csv
+end
+
+get '/location_names.csv' do
+	LOCATION_NAME_IMPORTER.pull
+	LOCATION_NAME_IMPORTER.to_csv
 end
