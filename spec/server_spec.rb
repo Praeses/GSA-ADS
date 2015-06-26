18697bc2512e1b63c87f2816d586c2aa4b6f154b
Glen Wallace <glen.wallace@praeses.com>
2015-06-26 15:06:44 -0500

    Adds tests to ensure that the URLs are able to be reached.

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -9,6 +9,25 @@ describe "The Sinatra Server" do
     last_response.should be_ok
   end
 
+  it "should be able to access ~/drug_counts.csv" do
+  	get '/drug_counts.csv'
+  	last_response.should be_ok
+	end
+
+	it "should be able to access ~/location_counts.csv" do
+  	get '/location_counts.csv'
+  	last_response.should be_ok
+	end
+
+	it "should be able to access ~/drug_name.csv" do
+		get '/drug_name.csv'
+  	last_response.should be_ok
+	end
+
+	# it "should be able to access ~/location_name.csv"
+		# get '/location_name.csv'
+  	# last_response.should be_ok
+	# end
 
 end
 
