581a6ae1dd0d1d02079cd80bfaed83a76863df27
Glen Wallace <glen.wallace@praeses.com>
2015-06-26 13:15:06 -0500

    Cleans up redundencies in the count_importer classes.

---
+++ src/services/count_importer_common.rb
@@ -0,0 +1,34 @@
+require 'open-uri'
+require 'json'
+require 'pry'
+require 'fileutils'
+require './src/services/abstract_importer.rb'
+
+module Services
+  module CountImporterCommon
+    def get_hash url, chem
+        JSON.parse(open(url).read)["results"].each do |result|
+        @unsaved[result["time"]] = {} unless @unsaved[result["time"]]
+        @unsaved[result["time"]][chem] = result["count"]
+      end
+      true
+    end
+
+    def get_csv list
+      csv = []
+     
+      @unsaved.each do |k,values|
+        row = [k]
+        list.each do |chem|
+          row << ( values[chem] || "0" )
+        end
+        
+        csv << row.to_csv
+
+      end
+      csv.sort!
+      csv = ["date," + list.join(",") + "\n"] + csv
+      csv.join('')
+    end
+  end
+end
